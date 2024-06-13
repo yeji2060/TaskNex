bfe3eca6da0d52996380b637bc165db75e41a2d4
ealhl <yayt.tuk@gmail.com>
2024-06-12 23:53:12 -0700

    add new task and edit task

--- react-front-end/src/routes.js
+++ react-front-end/src/routes.js
@@ -1,15 +1,21 @@
 // src/routes.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import TaskModule from './components/TaskModule'; // Make sure the path is correct
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import TaskModule from "./components/TaskModule";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/task/:id" element={<TaskModule open={true} onClose={() => {}} />} />
+        <Route
+          path="/task/:id"
+        />
+        {/* <Route
+          path="/task/:id"
+          element={<TaskModule open={true} onClose={() => false}/>}
+        /> */}
         {/* Add more routes as needed */}
       </Routes>
     </Router>
